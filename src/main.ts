247bc6a4173b825674dc56b08e3e8c58d22fcadd
Taiga2022 <f2021212010@gmail.com>
2023-11-26 16:13:22 +0000

    create Scene#7

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from "phaser";
+import IndexScene  from "./scene/index";
+import BrackJackScene from "./scene/game";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scale: Phaser.Types.Core.ScaleConfig = {
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  width: 1600,
+  height: 900
+};
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  scale: scale,
+  scene: [IndexScene,BrackJackScene],
+};
+
+new Phaser.Game(config);
---
+++ src/scene/game.ts
@@ -0,0 +1,22 @@
+import {Scene} from 'phaser';
+
+export default class BrackJackScene extends Scene {
+
+    constructor() {
+        super({ key: 'BrackJackScene', active: false });
+      }
+
+    preload(){
+      this.load.image("bg","assets/gameBg.png")
+    }
+
+    create(){
+      this.add.image(800, 450, 'bg');
+    }
+
+    update(){
+
+    }
+
+
+}
